81b57b0d76079ecea2db2ac64fbced82619aeb83
pipilapu558-hue <pipilapu558@gmail.com>
2026-08-29 14:15:31 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
+import express from "express";
 
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 10000;
+
+
+// หน้าแรก
+app.get("/", (req, res) => {
+
+    res.send("D-STATION LINE Webhook Server is running!");
+
+});
+
+
+// LINE Webhook
+app.post("/webhook", (req, res) => {
+
+    console.log(
+        "LINE Webhook:",
+        JSON.stringify(req.body, null, 2)
+    );
+
+    res.sendStatus(200);
+
+});
+
+
+app.listen(PORT, "0.0.0.0", () => {
+
+    console.log(
+        `Server running on port ${PORT}`
+    );
+
+});
