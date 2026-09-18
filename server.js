f127a550d0c0db8f1e0548c84208059e78f2fbfd
Pipi Lapu <pipilapu558@gmail.com>
2026-09-18 12:05:23 +0700

    Fix booking condition

--- server.js
+++ server.js
@@ -431,7 +431,6 @@ console.log("CHECK เบอร์ติดต่อ:", aiReply.includes("เบ
 console.log("CHECK เบอร์โทรศัพท์:", aiReply.includes("เบอร์โทรศัพท์"));
 
         if (
-    aiReply.includes("ข้อมูลครบแล้ว") &&
     (
         aiReply.includes("เบอร์ติดต่อ") ||
         aiReply.includes("เบอร์โทรศัพท์")
