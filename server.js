9be0f1348fb83bb557f9df8e0c2141c8e89210b3
Pipi Lapu <pipilapu558@gmail.com>
2026-09-21 15:56:27 +0700

    Fix CORS

--- server.js
+++ server.js
@@ -6,6 +6,29 @@ import fs from "fs";
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
+
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,POST,PUT,DELETE,OPTIONS"
+    );
+
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 10000;
